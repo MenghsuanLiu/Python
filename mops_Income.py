926a4de1d6a16c17daf17f1881b518ec09de7e23
Chris Liu <38637433+MenghsuanLiu@users.noreply.github.com>
2020-12-24 16:38:14 +0800

    Create mops_Income.py

---
+++ mops_Income.py
@@ -0,0 +1,34 @@
+import urllib.request as req
+import re as rex
+from bs4 import BeautifulSoup as bs
+
+# 年月
+# ym = ["109_9", "109_10", "109_11"]
+ym = "109_11"
+# 股票類別(sii = 上市, otc = 上櫃)
+stockcatg = ["sii", "otc"]
+
+head_info = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"}
+
+
+# url_tmp = "https://mops.twse.com.tw/nas/t21/{}/t21sc03_{}_0.html"
+url_tmp = "https://mops.twse.com.tw/nas/t21/{}/t21sc03_" + str(ym) + "_0.html"
+for catg in stockcatg:
+    url = url_tmp.format(catg)
+    urlwithhead = req.Request(url, headers = head_info)
+    # 取得網頁內容
+    with req.urlopen(urlwithhead) as respon:
+        webdata = respon.read()
+    #寫網頁原始碼到檔案中
+    root =  bs(webdata, "lxml")
+    with open ("web_source_" + catg + ".html", mode = "w", encoding = "UTF-8") as web_html:
+        web_html.write(root.prettify())
+    #取所有table
+    tb = root.find_all("table")
+    tb = root.findAll("th", text = rex.compile(".*半導體"))
+    print(tb)
+    break
+ 
+    # for a in tb:
+    #     tb_select = a.select("th > .tt", text = rex.compile("^產業別"))
+    #     print(tb_select)
