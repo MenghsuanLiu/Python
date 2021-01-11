132c6f88aa447a23ed971b6a689e8bdd678c0a48
Chris Liu <38637433+MenghsuanLiu@users.noreply.github.com>
2021-01-11 16:31:02 +0800

    Update mops_FinancialStatements202101.py

--- mops_FinancialStatements202101.py
+++ mops_FinancialStatements202101.py
@@ -1,13 +1,10 @@
 import os
-import re
-import urllib.request as req
+import requests as req
 from bs4 import BeautifulSoup as bs
 import pandas as pd
-import numpy as np
 import datetime
 from datequarter import DateQuarter
 import pyodbc as odbc
-
 
 # 取得網址相關資訊
 # url = "https://mops.twse.com.tw/server-java/t164sb01?step=1&CO_ID=3702&SYEAR=2019&SSEASON=3&REPORT_ID=C"
@@ -23,29 +20,45 @@ if os.path.exists(file_path) == False:
 if os.path.exists(web_path) == False:
     os.makedirs(web_path)
 
-StockList = ["2330", "2303"]
+StockList = ["2330", "2303", "5347", "2408", "2344", "2337", "6770"]
 # 以今天算出前一個季度
 PreviousQuarter = DateQuarter.from_date(datetime.date.today()) - 1
 year = str(PreviousQuarter._year)
 quarter = str(PreviousQuarter._quarter)
-quarter = "3"
-    
+quarter = "1"
+
+# 連結MS SQL資訊
+conn_sql = odbc.connect(Driver = '{SQL Server Native Client 11.0}', Server = "RAOICD01", database = "BIDC", user = "owner_sap", password = "oic#sap21o4")
+cursor = conn_sql.cursor() 
+
+
 record = 0
 data_head = []
 data_item = []
+YQ_FirstDate = []
+data_exist = ""
 for CompanyID in StockList:
     item_list = []
     # 處理網址
     url = url_templete.format(CompanyID, year, quarter)
-    urlwithhead = req.Request(url, headers = head_info)
+    urlwithhead = req.get(url, headers = head_info)
+    urlwithhead.encoding = "big5"
 
     # 取得網頁內容
-    with req.urlopen(urlwithhead) as respon:
-        webdata = respon.read().decode("big5")
+    # with req.urlopen(urlwithhead) as respon:
+    #     webdata = respon.read().decode("big5")
     # 寫網頁原始碼到檔案中
-    root =  bs(webdata, "lxml")
+    root =  bs(urlwithhead.text, "lxml")
     with open (web_path + "/FinancialWeb_" + CompanyID + "_" + year + "Q" + quarter + ".html", mode = "w", encoding = "UTF-8") as web_html:
         web_html.write(root.prettify())
+    # 判斷是否有資料
+    no_data = root.find("body").text
+    if no_data[0:5] == "檔案不存在":
+        print(CompanyID, ":", no_data)
+        continue
+    else:
+        data_exist = "X"    #一個loop有值就表示後面需要寫資料庫或Excel
+        
 
     # 取第一個table(BalanceSheet)
     tb_BalanceSheet = root.find_all("table")[0]
@@ -60,9 +73,9 @@ for CompanyID in StockList:
     with open (web_path + "/tb_CashFlow_" + CompanyID + "_" + year + "Q" + quarter + ".html", mode = "w", encoding = "UTF-8") as web_html:
         web_html.write(tb_CashFlow.prettify())
     # 取第四個table(Change in Equity)
-    tb_ChangeInEquity = root.find_all("table")[3]
-    with open (web_path + "/tb_ChangeInEquity_" + CompanyID + "_" + year + "Q" + quarter + ".html", mode = "w", encoding = "UTF-8") as web_html:
-        web_html.write(tb_ChangeInEquity.prettify())
+    # tb_ChangeInEquity = root.find_all("table")[3]
+    # with open (web_path + "/tb_ChangeInEquity_" + CompanyID + "_" + year + "Q" + quarter + ".html", mode = "w", encoding = "UTF-8") as web_html:
+    #     web_html.write(tb_ChangeInEquity.prettify())
     
     # 給固定值-Head(只要取一次就好)
     if record == 0:
@@ -75,28 +88,40 @@ for CompanyID in StockList:
     for fixval in (CompanyID, str(YQ_FirstDate)):
         item_list.append(fixval)
 
-    # 總資產(1XXX) / 總負債(2XXX)
-    for GL in ("1XXX", "2XXX"):        
-        # 抓第三個column,當季的值
+    # 總資產(1XXX) / 總負債(2XXX) / 普通股股本(3110)
+    for GL in ("1XXX", "2XXX", "3110"):        
+        ## 抓第三個column,當季的值
         item_val = tb_BalanceSheet.find("td", text = GL).find_parent("tr").find_all("td")[2].text.strip().replace(",", "").replace("(", "-").replace(")", "")        
-        item_list.append(int(item_val) * 1000)
-        # 抓第二個column,中文當成Header(只在第一個loop抓)
+        item_list.append(int(item_val) * 1000)       
+        ## 抓第二個column,中文當成Header(只在第一個loop抓)
         if record == 0:            
             head_val = tb_BalanceSheet.find("td", text = GL).find_parent("tr").find("span", class_ ="zh").text.strip()
-            data_head.append(head_val)
-    # 營業收入(4000) / 營業毛利(5950) / 營業費用(6000) / 其他收益(6500) / 營業利益(6900) / 營業外收入及支出(7000) / 母公司業主(8610) / 所得稅費用(7950)
+            data_head.append(head_val)        
+    # 營業收入(4000) / 營業毛利(5950) / 營業費用(6000) / 其他收益(6500) / 營業利益(6900) / 營業外收支(7000) / 母公司業主(8610) / 所得稅費用(7950)
     # 稅後淨利(8200) / 稀釋每股盈餘(9850) / 研究發展費用(6300)
     for GL in ("4000", "5950", "6000", "6500", "6900", "7000", "8610", "7950", "8200", "9850", "6300"):
-        # 抓第三個column,當季的值
-        item_val = tb_ComprehensiveIncome.find("td", text = GL).find_parent("tr").find_all("td")[2].text.strip().replace(",", "").replace("(", "-").replace(")", "")
+        ## 抓第三個column,當季的值
+        # 判斷是否有該GL Account(有才抓值,沒有就給0)
+        no_recod = ""
+        item_val = tb_ComprehensiveIncome.find("td", text = GL)
+        if item_val != None:
+            item_val = tb_ComprehensiveIncome.find("td", text = GL).find_parent("tr").find_all("td")[2].text.strip().replace(",", "").replace("(", "-").replace(")", "")
+        else:
+            item_val = 0
+            no_recod = "X"
+
         if GL == "9850":
             item_list.append(float(item_val))
         else:        
             item_list.append(int(item_val) * 1000)
-        # 抓第二個column,中文當成Header(只在第一個loop抓)
-        if record == 0:           
-            head_val = tb_ComprehensiveIncome.find("td", text = GL).find_parent("tr").find("span", class_ ="zh").text.strip()
+        ## 抓第二個column,中文當成Header(只在第一個loop抓)
+        if record == 0:
+            if no_recod != "X":         
+                head_val = tb_ComprehensiveIncome.find("td", text = GL).find_parent("tr").find("span", class_ ="zh").text.strip()
+            else:
+                head_val = GL    
             data_head.append(head_val)
+
     # 稅前淨利(A10000) / 利息費用(A20900) / 折舊費用(A20100) / 攤銷費用(A20200)
     for GL in ("A10000", "A20900", "A20100", "A20200"):
         # 抓第三個column,當季的值
@@ -106,29 +131,43 @@ for CompanyID in StockList:
         if record == 0:
             head_val = tb_CashFlow.find("td", text = GL).find_parent("tr").find("span", class_ ="zh").text.strip()
             data_head.append(head_val)
-    # 普通股股本(3110)    
-    item_val = tb_ChangeInEquity.find("td", text = "Z1").find_parent("tr").find_all("td")[2].text.strip().replace(",", "").replace("(", "-").replace(")", "")
-    item_list.append(int(item_val) * 1000)
-    # Head手動給(只在第一個loop做)
-    if record == 0:
-        data_head.append("普通股股本")
+    # # 普通股股本(3110)    
+    # item_val = tb_ChangeInEquity.find("td", text = "Z1").find_parent("tr").find_all("td")[2].text.strip().replace(",", "").replace("(", "-").replace(")", "")
+    # item_list.append(int(item_val) * 1000)
+    # # Head手動給(只在第一個loop做)
+    # if record == 0:
+    #     data_head.append("普通股股本")
 
     record += 1
     data_item.append(item_list)
 
-df_Financial = pd.DataFrame(data_item, columns = data_head)
-df_Financial.to_excel(file_path + "/Financial.xlsx", index=False)
-# print(df_Financial)
-quit()
+if data_exist == "X":
+    ## 產生Excel的部份
+    df_Financial = pd.DataFrame(data_item, columns = data_head)
+    df_Financial.to_excel(file_path + "/Financial.xlsx", index=False)
+    # print(df_Financial)
+
+    ## 處理資料庫的部份
+    # 先刪資料
+    SQL_Delete = ("DELETE FROM BIDC.dbo.mopsFinancialByCompany WHERE YQ_Date = '" + str(YQ_FirstDate) + "'")
+
+    cursor.execute(SQL_Delete)
+    conn_sql.commit()
+    # 寫資料到MS SQL(Revenue)
+    SQL_Insert = ("INSERT INTO BIDC.dbo.mopsFinancialByCompany (YQ_Date, StockID, Assets, Liabilities, Oper_Revenue, GP_Oper, Oper_Expenses, NetOtherIncome, NetOperIncome, nonOperIncome, PF_BeforeTax, Profit, PF_AttrOwners, Inter_Expense, Tax_Expense, DP_Expense, Amor_Expense, EPS, RD_Expense, OrdinaryShare) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+    # Insart資料
+    for list in data_item:
+        # list[1]=日期, [0]=公司, [2]=總資產, [3]=總負債, [5]=營業收入, [6]=營業毛利, [7]=營業費用, [8]營業費用(其他), [9]營業利益, [10]營業外收支, [16]稅前淨利, [13]稅後淨利, [11]稅後純益(損), [17]利息費用, [12]所得稅費用, [18]折舊費用, [19]攤銷費用, [14]EPS, [15]RD, [4]流通在外張數
+        value = [ list[1], list[0], list[2], list[3], list[5], list[6], list[7], list[8], list[9], list[10], list[16], list[13], list[11], list[17], list[12], list[18], list[19], list[14], list[15], list[4] ]
+        cursor.execute(SQL_Insert, value)
+        conn_sql.commit()
+
+    conn_sql.close()
+    print("Financial Data Update Complete!!")
 
 """ 
-
-
-
-
 ymd = GL = GL_Desc =  data_list =[]
 GL_Level = amtvalue = 0
-
 # 從第二個Row開始loop起(Row 1 = 資產負債表)
 for rows in tb_BalanceSheet.select("tr")[1:]:
     # 抓出年月日(表頭,抓第三欄為指定季的最後一天)
@@ -171,7 +210,6 @@ for rows in tb_BalanceSheet.select("tr")[1:]:
         data_list.append(list)
 # df_BalanceSheet = pd.DataFrame(data_list)
 # print(df_BalanceSheet)
-
 # 從第三個Row開始loop起(Row 1 = 綜合損益表)
 for rows in tb_ComprehensiveIncome.select("tr")[2:]:
     # 抓資料的Column 1[會科]
@@ -200,13 +238,10 @@ for rows in tb_ComprehensiveIncome.select("tr")[2:]:
         # print(company, "\t", ymd, "\t", GL if GL != None else " " , "\t", GL_Level, GL_Desc, "\t", float(amtvalue))        
         list = [str(company), ymd, GL if GL != None else " ", GL_Desc, GL_Level, float(amtvalue)]
         data_list.append(list)
-
 conn_sql = odbc.connect(Driver = '{SQL Server Native Client 11.0}', Server = "RAOICD01", database = "BIDC", user = "owner_sap", password = "sap@@20166")
 cursor = conn_sql.cursor()
 SQL_Insert = ("INSERT INTO BIDC.dbo.mopsGLAccountDataByCompany (StockID, ReportDate, GLAccount, GLAccountDesc, Hierarchy, Amount) VALUES (?, ?, ?, ?, ?, ? );")
 SQL_Delete = ("DELETE FROM BIDC.dbo.mopsGLAccountDataByCompany WHERE ReportDate = '" + ymd + "' AND StockID = '" + str(company) +"'")
-
-
 # 先刪資料
 cursor.execute(SQL_Delete)
 conn_sql.commit()
