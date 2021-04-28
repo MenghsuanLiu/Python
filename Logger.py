e55f3e2e05191e2fa2c8a22dad168243fd71f6e8
Chris Liu <38637433+MenghsuanLiu@users.noreply.github.com>
2021-04-28 09:04:52 +0800

    Create Logger.py

---
+++ Logger.py
@@ -0,0 +1,32 @@
+import logging
+import os
+from datetime import datetime
+
+
+# dir_path = 'logs/'   # 設定 logs 目錄
+filename = "{:%Y%m%d_%H%M}".format(datetime.now()) + '.log'     
+# 設定檔名
+
+def create_logger(log_folder):
+    # config
+    logging.captureWarnings(True)   # 捕捉 py waring message
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    my_logger = logging.getLogger('py.warnings')    # 捕捉 py waring message
+    my_logger.setLevel(logging.INFO)
+ 
+    # 若不存在目錄則新建
+    if not os.path.exists(log_folder):
+        os.makedirs(log_folder)
+ 
+    # file handler
+    fileHandler = logging.FileHandler(log_folder + '/' + filename, 'w', 'utf-8')
+    fileHandler.setFormatter(formatter)
+    my_logger.addHandler(fileHandler)
+ 
+    # console handler
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setLevel(logging.DEBUG)
+    consoleHandler.setFormatter(formatter)
+    my_logger.addHandler(consoleHandler)
+ 
+    return my_logger
