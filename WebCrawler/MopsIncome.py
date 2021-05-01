4e7930a71bdcc214a8d8a98def356f2b033dc577
Chris Liu <38637433+MenghsuanLiu@users.noreply.github.com>
2021-05-01 08:28:58 +0800

    modify

--- WebCrawler/MopsIncome.py
+++ WebCrawler/MopsIncome.py
@@ -64,8 +64,9 @@ def check_CompExist(comp_df, chk_stockid):
         shownname = df_list["EnShowName"].values[0]
     return status, shownname
 # 針對PSMC要計算L及M的當月revenue(計算LSPF的,Memory用扣的)
-def splitPSMCRevenueByBU(list):
-    if list[1] == "6770":
+def splitPSMCRevenueByBU(list, updb):
+    revenueval = 0
+    if updb != "" and list[1] == "6770":
         ym = list[0]
         ym = ym.split("-")[0] + str(ym.split("-")[1] if int(ym.split("-")[1]) >= 10 else "0" + ym.split("-")[1]) +"%"
 
@@ -82,8 +83,6 @@ def splitPSMCRevenueByBU(list):
                                             WHERE FKDAT LIKE {date1}
                                         ) as a""".format(type1 = "F2", type2 = "L2", date1 = ym) )
                 revenueval = round([float(r[0]) for r in cursor.fetchall()][0] / 1000, 0)
-    else:
-        revenueval = 0
     return revenueval
 # 取得欄位中的值
 def get_tbColval(rowlist, colid):
@@ -133,14 +132,16 @@ def writeExcel(DataH, DataI, fname, genxls):
         #     # df_imcome.to_csv(file_path + "/" + file_name + "_" + en.replace("-", "") + ".csv", encoding = en, index = False )
         #     df_imcome.to_csv(file_path + "/revenue_" + en.replace("-", "") + ".csv", encoding = en, index = False )
         
-def getComplist_mssql():
-    cols = ["StockID", "StockName", "Market", "Industry", "EnShowName"]
-    with pymssql.connect( server = "RAOICD01", user = "owner_sap", password = "oic#sap21o4", database = "BIDC" ) as conn:
-        with conn.cursor() as cursor:
-            cursor.execute("SELECT StockID, StockName, Market, Industry, EnShowName FROM BIDC.dbo.mopsStockCompanyInfo")
-            quary = cursor.fetchall()
-            df_list = pd.DataFrame(quary, columns = cols)
-    return df_list
+def getComplist_mssql(db):
+
+    if db != "":
+        cols = ["StockID", "StockName", "Market", "Industry", "EnShowName"]
+        with pymssql.connect( server = "RAOICD01", user = "owner_sap", password = "oic#sap21o4", database = "BIDC" ) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("SELECT StockID, StockName, Market, Industry, EnShowName FROM BIDC.dbo.mopsStockCompanyInfo")
+                quary = cursor.fetchall()
+                df_list = pd.DataFrame(quary, columns = cols)
+        return df_list
 
 def getMonthFromToday(num):
     num = num * -1
@@ -154,20 +155,25 @@ def getMonthFromToday(num):
 cfg_fname = "./config/config.json"
 web_path = "./data/html_file"
 dict_catg = {"sii": "上市公司", "otc": "上櫃公司", "rotc": "興櫃公司", "pub": "公開發行公司"}
+
 # 取得需要抓取的年月清單
 try:
     ym = getConfigData(cfg_fname, "yearmon")
 except:
     ym = getMonthFromToday(-1)  #-1 往前一個月
 # 取得市場類別的清單
+
 stockcatg = getConfigData(cfg_fname, "stocktype") # stockcatg = ["sii", "otc", "rotc", "pub"]
+
 # 取得產業別的清單
 industy = getConfigData(cfg_fname, "industygroup") # industy = ["半導體", "電子工業"]
-# 取得公司的清單(先前已存在資料庫中)
-# df_Complist = getComplist_mssql()
 # 判斷是否需要產生Excel File
 up_xlsx = getConfigData(cfg_fname, "update_xls")
-
+# 判斷是否Update DB
+up_db = getConfigData(cfg_fname, "update_db")
+# 取得公司的清單(先前已存在資料庫中)
+df_Complist = getComplist_mssql(up_db)
+quit()
 
 
 # %%
@@ -208,10 +214,10 @@ for catg in stockcatg:
             if data_head == []:
                 data_head = get_Header(tb)
 
-# 取公司類別
+            # 取公司類別
             cmpindusty = get_CompIndustry(tb)
 
-# Get Item =>從第3個Row開始loop起(Row 3 以後是資料)
+            # Get Item =>從第3個Row開始loop起(Row 3 以後是資料)
             for rows in tb.select("table > tr")[2:]:
                 StockID = get_tbColval(rows, 1)
                 StockName = get_tbColval(rows, 2)
@@ -230,40 +236,39 @@ for catg in stockcatg:
                 if DiffPercent == "":
                     DiffPercent = 0
                 Remark = get_tbColval(rows, 11)
-
-
-                collect = [yyyymm, StockID, StockName, int(CurrRevenue), int(LastRevenue), int(YoYRevenue), float(LastPercent), float(YoYPercent), int(CurrCount), int(LastCount), float(DiffPercent), Remark, market]
-                data_item.append(collect)
-                # if StockID != "":
-                #     key = [yyyymm, StockID]
-                #     if key not in key_list:
-                #         collect = [yyyymm, StockID, StockName, int(CurrRevenue), int(LastRevenue), int(YoYRevenue), float(LastPercent), float(YoYPercent), int(CurrCount), int(LastCount), float(DiffPercent), Remark, market]
-                #         # 遇到PSMC要拆BU
-                #         psmc_lspf_val = splitPSMCRevenueByBU(collect)
-                #         if psmc_lspf_val == 0:
-                #             collect.append("")
-                #         else:
-                #             collect_tmp = collect.copy()
-                #             psmc_m_val = collect[3] - psmc_lspf_val
-                #             collect_tmp[3] = psmc_lspf_val
-                #             collect_tmp.append("L")
-                #             data_item.append(collect_tmp)
-                #             collect[3]  = psmc_m_val
-                #             collect.append("M")
-                #         data_item.append(collect)
-                #         key_list.append(key)
+                # 收集資料放到data_item,同時若遇到PSMC就要去拆BU計算
+                if StockID != "":
+                    key = [yyyymm, StockID]
+                    if key not in key_list:
+                        collect = [yyyymm, StockID, StockName, int(CurrRevenue), int(LastRevenue), int(YoYRevenue), float(LastPercent), float(YoYPercent), int(CurrCount), int(LastCount), float(DiffPercent), Remark, market]
+                        # 遇到PSMC要拆BU
+                        psmc_lspf_val = splitPSMCRevenueByBU(collect, up_db)
+                        if psmc_lspf_val == 0:
+                            collect.append("")
+                        else:
+                            collect_tmp = collect.copy()
+                            psmc_m_val = collect[3] - psmc_lspf_val
+                            collect_tmp[3] = psmc_lspf_val
+                            collect_tmp.append("L")
+                            data_item.append(collect_tmp)
+                            collect[3]  = psmc_m_val
+                            collect.append("M")
+                        data_item.append(collect)
+                        key_list.append(key)
 
                     
-                    
-                #     collect = [StockID, StockName, market, cmpindusty]
-                #     if collect not in data_company:
-                #         chk, shown = check_CompExist(df_Complist, collect)
-                #         if chk == "append":
-                #             collect.append("")
-                #             data_company.append(collect)    
-                #         if chk == "modify":
-                #             collect.append(shown)
-                #             data_company.append(collect)
+                    # 收集公司清單的資料
+                    collect = [StockID, StockName, market, cmpindusty]
+                    if collect not in data_company:
+                        chk, shown = check_CompExist(df_Complist, collect)
+                        if chk == "append":
+                            collect.append("")
+                            data_company.append(collect)    
+                        if chk == "modify":
+                            collect.append(shown)
+                            data_company.append(collect)
+                    print(collect)
+                    quit()
 
 
 
