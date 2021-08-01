b754e364c5143c161296715fbde63bf69c7cf14f
MenghsuanLiu <chris0325@gmail.com>
2021-08-01 12:02:45 +0800

    vscode setting

---
+++ CNN.py
@@ -0,0 +1,13 @@
+# Build a CNN Model From mnist Dataset
+# https://www.youtube.com/watch?v=RPOlHiMHZTE
+# %%
+import tensorflow as tf
+
+data = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = data.load_data()
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+x_train, x_test = x_train / 255, x_test / 255
+y_train = tf.keras.utils.to_categorical(y_train, 10)
+y_test = tf.keras.utils.to_categorical(y_test, 10)
+# %%
