9f13ccaf16d50ebd9285108cd953ddde5021edfa
MenghsuanLiu <chris0325@gmail.com>
2022-12-23 21:05:28 +0800

    fix script

--- GUIWinningList.py
+++ GUIWinningList.py
@@ -3,9 +3,7 @@ import requests
 import pandas as pd
 import numpy as np
 import time
-import os
 from datetime import date
-from datetime import datetime
 from dateutil.relativedelta import relativedelta
 # import pyrfc as rfc
 
@@ -33,6 +31,7 @@ def calYearMonthWhichIsEven(baseday: date = date.today(), step: int = 0, ce: boo
 if __name__ == "__main__":
     map_dict = {"super": "T", "spc": "S", "first": "1", "second": "2", "third": "3", "fourth": "4", "fifth": "5", "sixth": "6"}
     setp_val = -2
+    wait = 10
 
 
     # 取得中獎號的期號(YYYMM => YYY為民國年 MM是偶數)
@@ -43,7 +42,7 @@ if __name__ == "__main__":
         exit()
     
     # conn = rfc.Connection(user = "MISSD", passwd = "test2018", ashost = "172.20.97.81", sysnr = "00", client = "300")
-    # chk_exist = bool(conn.call("ZRFC_GET_GUI_WINNING_LIST", I_GGJAH = "2022", I_GMONA = "10").get("E_EXIST"))
+    # chk_exist = bool(conn.call("ZRFC_GET_GUI_WINNING_LIST", I_GGJAH = yyyy, I_GMONA = mm).get("E_EXIST"))
     # if chk_exist:
     #     print("資料己存在!!!")
     #     conn.close()
@@ -54,47 +53,46 @@ if __name__ == "__main__":
     para_win = {"version": "0.2", "action": "QryWinningList", "invTerm": ym, "appID": "EINV7201706312375", "UUID": "https://github.com/jasonlamkk/OpenUDID.Net"}
     url_win = "https://api.einvoice.nat.gov.tw/PB2CAPIVAN/invapp/InvApp"
 
-    # try:
-    r = requests.post( url_win, headers = head, params = para_win )
-    if int(r.json().get("code")) != 200:
-        print(r.json().get("msg"))
-        exit()
+    while True:
+        try:
+            r = requests.post( url_win, headers = head, params = para_win )
+            if int(r.json().get("code")) != 200:
+                print(r.json().get("msg"))
+                exit()
 
-    time.sleep(5)
-    df = pd.json_normalize(r.json(), max_level=1)
+            time.sleep(5)
+            df = pd.json_normalize(r.json(), max_level=1)
 
-    df_prize_o = df.filter(regex="PrizeNo").replace("", np.nan).T.rename({0: "PIZNO"},axis = 1).dropna().rename_axis("pizname").reset_index()
-    df_amt = df.filter(regex = "Amt").T.rename({0: "PZAMT"}, axis = 1).astype({"PZAMT": int}).rename_axis("pizname").reset_index()
-    df_prize_o["PRTYP"] = df_prize_o["pizname"].str.split("Prize", expand = True)[0].map(map_dict)
-    df_prize_o["ADDON"] = df_prize_o["pizname"].str.split("Prize", expand = True)[0].map({"sixth": "X"})
-    df_amt["PRTYP"] = df_amt["pizname"].str.split("Prize", expand = True)[0].map(map_dict)
-    # 用頭獎做出另外五組
-    df_prize = pd.DataFrame()
-    for i in range(2,7):
-        a = df_prize_o[df_prize_o.pizname.str.match("first")]
-        a["PRTYP"] = str(i)
-        a["PIZNO"] = a["PIZNO"].str.slice(i - 1, 8)
-        df_prize = df_prize.append(a)
-    df_prize = df_prize.append(df_prize_o).reset_index(drop=True).replace(np.nan, "")
-    df_prize = df_prize.merge(df_amt, on = "PRTYP", how = "left")
-    df_prize = df_prize.drop(list(df_prize.filter(regex = "pizname")), axis=1).sort_values(by = "PRTYP").reset_index(drop=True)
-    df_prize["WAERK"] = "TWD"
-    df_prize["RECOD"] = range(1,len(df_prize)+1)
-    df_prize["ERDAT"] = datetime.now().strftime("%Y%m%d")
-    df_prize["ERZET"] = datetime.now().strftime("%H%M%S")
-    df_prize["CPROG"] = os.path.basename(__file__)
-    df_prize["GGJAH"], df_prize["GMONA"] = yyyy, mm
-    df_prize = df_prize.astype(str)
+            df_prize = df.filter(regex="PrizeNo").replace("", np.nan).T.rename({0: "PIZNO"},axis = 1).dropna().rename_axis("pizname").reset_index()
+            df_amt = df.filter(regex = "Amt").T.rename({0: "PZAMT"}, axis = 1).astype({"PZAMT": int}).rename_axis("pizname").reset_index()
+            df_prize["PRTYP"] = df_prize["pizname"].str.split("Prize", expand = True)[0].map(map_dict)
+            df_prize["ADDON"] = df_prize["pizname"].str.split("Prize", expand = True)[0].map({"sixth": "X"})
+            df_amt["PRTYP"] = df_amt["pizname"].str.split("Prize", expand = True)[0].map(map_dict)
+            
+            # 用頭獎做出另外五組
+            df_update = pd.DataFrame()
 
-    print(df_prize)
+            for i in range(2,7):
+                a = df_prize[df_prize["PRTYP"]==str(1)].replace("1", str(i))
+                a.PIZNO = a.PIZNO.str.slice(i - 1, 8)
+                # a = df_prize[df_prize.pizname.str.match("first")]
+                # a["PRTYP"] = str(i)
+                # a["PIZNO"] = a["PIZNO"].str.slice(i - 1, 8)
+                df_update = pd.concat([df_update, a], ignore_index = True)
+            df_update = pd.concat([df_update, df_prize], ignore_index = True).replace(np.nan, "")
+            # df_update = df_update.append(df_prize).reset_index(drop=True).replace(np.nan, "")
+            df_update = df_update.merge(df_amt, on = "PRTYP", how = "left")
+            df_update = df_update.drop(list(df_update.filter(regex = "pizname")), axis=1).sort_values(by = "PRTYP").reset_index(drop=True)
+            df_update = df_update.astype(str)
+            break
+        except:
+            time.sleep(wait)
+            wait += 10
+            print(f"下次執行時間為{wait}秒後~~~")
+            continue
 
-
-
+    print(df_update)
     
-    # # a = [{"GGJAH": "2022", "GMONA": "08", "RECOD": "1", "PRTYP": "T", "PIZNO": "11174120", "PZAMT": "10000000", "WAERK": "TWD"},
-    # #      {"GGJAH": "2022", "GMONA": "08", "RECOD": "2", "PRTYP": "S", "PIZNO": "59276913", "PZAMT": "2000000", "WAERK": "TWD"}]
-    # result = conn.call("ZRFC_GET_GUI_WINNING_LIST", I_GGJAH = "2022", I_GMONA = "10", T_DATA = df_prize.to_dict("records"))
+    # result = conn.call("ZRFC_GET_GUI_WINNING_LIST", I_GGJAH = yyyy, I_GMONA = mm, T_DATA = df_update.to_dict("records"))
     # print(result)
     # conn.close()
-
-# %%
