52653b103b585a2bd98863d85dbc87c9b3301e18
MenghsuanLiu <chris0325@gmail.com>
2022-12-25 20:05:18 +0800

    20221225 debug

--- GUIWinningList.py
+++ GUIWinningList.py
@@ -31,7 +31,7 @@ def calYearMonthWhichIsEven(baseday: date = date.today(), step: int = 0, ce: boo
 if __name__ == "__main__":
     map_dict = {"super": "T", "spc": "S", "first": "1", "second": "2", "third": "3", "fourth": "4", "fifth": "5", "sixth": "6"}
     setp_val = -2
-    wait = 10
+    wait = 0
 
 
     # 取得中獎號的期號(YYYMM => YYY為民國年 MM是偶數)
@@ -52,7 +52,7 @@ if __name__ == "__main__":
     head = {"Content-Type": "application/x-www-form-urlencoded"}  
     para_win = {"version": "0.2", "action": "QryWinningList", "invTerm": ym, "appID": "EINV7201706312375", "UUID": "https://github.com/jasonlamkk/OpenUDID.Net"}
     url_win = "https://api.einvoice.nat.gov.tw/PB2CAPIVAN/invapp/InvApp"
-
+# %%
     while True:
         try:
             r = requests.post( url_win, headers = head, params = para_win )
@@ -60,35 +60,30 @@ if __name__ == "__main__":
                 print(r.json().get("msg"))
                 exit()
 
-            time.sleep(5)
             df = pd.json_normalize(r.json(), max_level=1)
-
             df_prize = df.filter(regex="PrizeNo").replace("", np.nan).T.rename({0: "PIZNO"},axis = 1).dropna().rename_axis("pizname").reset_index()
             df_amt = df.filter(regex = "Amt").T.rename({0: "PZAMT"}, axis = 1).astype({"PZAMT": int}).rename_axis("pizname").reset_index()
             df_prize["PRTYP"] = df_prize["pizname"].str.split("Prize", expand = True)[0].map(map_dict)
+            # 如果有增開六獎就在ADDON欄位給值
             df_prize["ADDON"] = df_prize["pizname"].str.split("Prize", expand = True)[0].map({"sixth": "X"})
             df_amt["PRTYP"] = df_amt["pizname"].str.split("Prize", expand = True)[0].map(map_dict)
-            
-            # 用頭獎做出另外五組
+            # 用頭獎做出另外五組,同時取獎號後幾碼(二獎=>7碼...六獎=>3碼)
             df_update = pd.DataFrame()
-
             for i in range(2,7):
-                a = df_prize[df_prize["PRTYP"]==str(1)].replace("1", str(i))
+                a = df_prize[df_prize["PRTYP"] == str(1)].replace("1", str(i))
                 a.PIZNO = a.PIZNO.str.slice(i - 1, 8)
-                # a = df_prize[df_prize.pizname.str.match("first")]
-                # a["PRTYP"] = str(i)
-                # a["PIZNO"] = a["PIZNO"].str.slice(i - 1, 8)
                 df_update = pd.concat([df_update, a], ignore_index = True)
+            # 和原來的特獎~一獎(增開六獎)組在一起
             df_update = pd.concat([df_update, df_prize], ignore_index = True).replace(np.nan, "")
-            # df_update = df_update.append(df_prize).reset_index(drop=True).replace(np.nan, "")
+            # 串出各獎金額
             df_update = df_update.merge(df_amt, on = "PRTYP", how = "left")
             df_update = df_update.drop(list(df_update.filter(regex = "pizname")), axis=1).sort_values(by = "PRTYP").reset_index(drop=True)
             df_update = df_update.astype(str)
             break
         except:
-            time.sleep(wait)
             wait += 10
             print(f"下次執行時間為{wait}秒後~~~")
+            time.sleep(wait)
             continue
 
     print(df_update)
