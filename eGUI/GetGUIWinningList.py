9446480301d94767712b1103004142c9070a23af
MenghsuanLiu <chris0325@gmail.com>
2023-11-28 21:48:08 +0800

    New Project eGUI

--- eGUI/GetGUIWinningList.py
+++ eGUI/GetGUIWinningList.py
@@ -1,16 +1,19 @@
 # %%
+import os
 import requests
 import pandas as pd
 import numpy as np
 import time
 import logging
+import json
 from datetime import date
 from dateutil.relativedelta import relativedelta
 import pyrfc as rfc
+
 # %%
 
 # 計算年月值(網頁參數)baseday=>基準日, step=>-1往前一個單位
-def calYearMonthWhichIsEven(baseday: date = date.today(), step: int = 0, ce: bool = True)-> None:
+def calYearMonthWhichIsEven(baseday: date = date.today(), step: int = -1, ce: bool = True)-> None:
     
     caldate = baseday + relativedelta(months = step)
     if(int(caldate.month) % 2) == 0:
@@ -26,42 +29,58 @@ def calYearMonthWhichIsEven(baseday: date = date.today(), step: int = 0, ce: boo
             else:
                 return str(caldate.year - 1911) + str(caldate.month)
     else:
-        return False
+        return 0
 
 
 if __name__ == "__main__":
     # Log的相闗設定
     yyyymmdd = date.today().strftime("%Y%m%d")
-    logging.basicConfig(filename = f".\GUI_WinningList_{yyyymmdd}.log", level = logging.DEBUG, 
-                        format='%(asctime)s %(levelname)s %(name)s %(message)s')
-    logger = logging.getLogger(__name__)
-
-    sapconn = ["PSMCERP", "SAP#MAX0", "172.16.6.232", "00", "800"]
-    # sapconn = ["MISSD", "test2018", "172.20.97.81", "00", "300"]
-    map_dict = {"super": "T", "spc": "S", "first": "1", "second": "2", "third": "3", "fourth": "4", "fifth": "5", "sixth": "6"}
+    curpath = os.path.dirname(__file__)
+    sid = "PRD"
     setp_val = -1
     wait = 0
+    logging.basicConfig(filename = f"{curpath}\log\GUI_WinningList_{yyyymmdd}.log", level = logging.DEBUG, 
+                        format='%(asctime)s %(levelname)s %(name)s %(message)s', encoding = "utf-8")
+    logger = logging.getLogger(__name__)
+    logger.info("~~~~~~~Program started~~~~~~~")
+    # 取得SAP連線資訊
+    sapconn = json.load(open(f"{curpath}\config\connect.json"))[sid]
+    map_dict = json.load(open(f"{curpath}\config\connect.json"))["winmap"]
+    
+    logger.info(f"SAP連線資訊({sid}):{sapconn}")
 
-
+    # sapconn = ["PSMCERP", "SAP#MAX0", "172.16.6.232", "00", "800"]
+    # sapconn = ["MISSD", "test2018", "172.20.97.81", "00", "300"]
+    # map_dict = {"super": "T", "spc": "S", "first": "1", "second": "2", "third": "3", "fourth": "4", "fifth": "5", "sixth": "6"}
     # 取得中獎號的期號(YYYMM => YYY為民國年 MM是偶數)
     ym = calYearMonthWhichIsEven(step = setp_val, ce = False)
-    # ym出來時是False就不往下走了
-    if not ym:
+    # ym出來時是0就不往下走了
+    if ym == 0:
+        logger.error(f"以今天為Base,往前/後{abs(setp_val)}個月,不是偶數月,程式將停止!")
         print("Exit Program!!")
         exit()
+    # 取得要抓中獎發票的年月    
     yyyy, mm = calYearMonthWhichIsEven(step = setp_val)
-    
-    conn = rfc.Connection(user = sapconn[0], passwd = sapconn[1], ashost = sapconn[2], sysnr = sapconn[3], client = sapconn[4])
+    logger.info(f"取得的執行年月為:{yyyy},{mm}")
+    # 建立連線,檢查資料是否己經存在
+    conn = rfc.Connection(user = sapconn["uname"], passwd = sapconn["pwd"], ashost = sapconn["host"], sysnr = sapconn["sysnr"], client = sapconn["client"])
+    # conn = rfc.Connection(user = sapconn[0], passwd = sapconn[1], ashost = sapconn[2], sysnr = sapconn[3], client = sapconn[4])
     chk_exist = bool(conn.call("ZRFC_GET_GUI_WINNING_LIST", I_GGJAH = yyyy, I_GMONA = mm).get("E_EXIST"))
     conn.close()
     if chk_exist:
+        logger.error(f"{yyyy}/{mm}的中獎發票資料己存在於SAP中,程式將停止!")
         print("資料己存在!!!")
         exit()
-    # 連線參數
-    head = {"Content-Type": "application/x-www-form-urlencoded"}  
-    para_win = {"version": "0.2", "action": "QryWinningList", "invTerm": ym, "appID": "EINV7201706312375", "UUID": "https://github.com/jasonlamkk/OpenUDID.Net"}
+
+    # 取得Web連線參數
+    head = json.load(open(f"{curpath}\config\connect.json"))["head"]
+    url_win = json.load(open(f"{curpath}\config\connect.json"))["url"]
+    para_win = json.load(open(f"{curpath}\config\connect.json"))["parameter"]
+    para_win["invTerm"] = ym
+    # head = {"Content-Type": "application/x-www-form-urlencoded"}  
+    # para_win = {"version": "0.2", "action": "QryWinningList", "invTerm": ym, "appID": "EINV7201706312375", "UUID": "https://github.com/jasonlamkk/OpenUDID.Net"}
     # para_win = {"version": "0.2", "action": "qryLoveCode", "qKey": "88432", "appID": "EINV7201706312375", "UUID": "https://github.com/jasonlamkk/OpenUDID.Net"}
-    url_win = "https://api.einvoice.nat.gov.tw/PB2CAPIVAN/invapp/InvApp"
+    # url_win = "https://api.einvoice.nat.gov.tw/PB2CAPIVAN/invapp/InvApp"
     # url_win = "https://api.einvoice.nat.gov.tw/PB2CAPIVAN/loveCodeapp/qryLoveCode"
 
     while True:
@@ -95,12 +114,16 @@ if __name__ == "__main__":
             logger.error(e)
             wait += 10
             print(f"下次執行時間為{wait}秒後~~~")
+            logger.info(f"下次執行時間為{wait}秒後~~~")
             time.sleep(wait)
             continue
 
     print(df_update)
-    conn = rfc.Connection(user = sapconn[0], passwd = sapconn[1], ashost = sapconn[2], sysnr = sapconn[3], client = sapconn[4])
+    logger.info(df_update)
+    conn = rfc.Connection(user = sapconn["uname"], passwd = sapconn["pwd"], ashost = sapconn["host"], sysnr = sapconn["sysnr"], client = sapconn["client"])
+    # conn = rfc.Connection(user = sapconn[0], passwd = sapconn[1], ashost = sapconn[2], sysnr = sapconn[3], client = sapconn[4])
     result = conn.call("ZRFC_GET_GUI_WINNING_LIST", I_GGJAH = yyyy, I_GMONA = mm, T_DATA = df_update.to_dict("records"))
     print(result)
+    logger.info(result)
     conn.close()
-# %%
+    logger.info("~~~~~~~Program End~~~~~~~")
